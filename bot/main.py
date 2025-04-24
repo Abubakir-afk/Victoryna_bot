30b8290c2f21065ae0b64697dcb8bc90e49db668
Abubakr <abubakirzoki@gmail.com>
2025-04-24 09:24:21 +0300

    added session

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,9 @@ from aiogram import Bot, Dispatcher
 from bot.menu import set_bot_menu
 from handlers import router as handlers_router
 
+from aiogram.client.session.aiohttp import AiohttpSession
+session = AiohttpSession(proxy="http://proxy.server:3128")
+
 load_dotenv()
 
 TOKEN = getenv("BOT_TOKEN")
@@ -13,7 +16,7 @@ dp = Dispatcher()
 dp.include_router(handlers_router)
 
 async def main() -> None:
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, session=session)
     await set_bot_menu(bot)
     await dp.start_polling(bot)
 
